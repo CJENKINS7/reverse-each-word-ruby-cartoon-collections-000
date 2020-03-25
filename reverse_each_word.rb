9e7c599b93facf4ba7243456a3c89e5673b1b387
congenial-principle-5422 <christinajenkins10@gmail.com>
2020-03-25 22:04:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 words_reversed = ["olleH, ereht dna woh era ?uoy"]
 
 def reverse_each_word(words)
-
-
+words.collect do |x|
+end
 
 reverse_each_word("Hello there, and how are you?")
   #=> "olleH ,ereht dna woh era ?uoy"
